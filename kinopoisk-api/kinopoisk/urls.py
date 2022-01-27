97ab9a143be33535ebc9a68a78d608a4018b2190
Александр <samitist@mail.ru>
2022-01-28 02:19:27 +0300

    add movie

--- kinopoisk-api/kinopoisk/urls.py
+++ kinopoisk-api/kinopoisk/urls.py
@@ -6,7 +6,8 @@ from .views import django_recommendation
 is_fill_needed()
 
 urlpatterns = [
-    re_path(r'api/recommendation/.*', django_recommendation)
+    re_path(r'api/gettingmovie/.*', django_recommendation),
+    re_path(r'api/')
 ]
 
 #Initialization
